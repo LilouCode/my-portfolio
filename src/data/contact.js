24c24a6a99cb5993b9a42b282a3999db1aec82a3
LilouCode <lisa.music@outlook.com>
2022-07-27 21:36:19 +0200

    feat: create data

---
+++ src/data/contact.js
@@ -0,0 +1,10 @@
+const contact = 
+    {
+        name: "Lisa Lox",
+        email: "lisa.frontdev@gmail.com",
+        github:"https://github.com/LilouCode",
+        place: "remote"
+        
+    }
+
+export default contact
