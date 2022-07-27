c116136e0f1c1429d48b7234c3a2465a8e54e069
LilouCode <lisa.music@outlook.com>
2022-07-27 09:03:25 +0200

    feat: create all pages

--- src/component/pages/Home.jsx
+++ src/component/pages/Home.jsx
@@ -1,5 +1,17 @@
-export const Home = () =>{
-    return(
-        <div>HELLO</div>
-    )
-}
+import { Link } from "react-router-dom";
+import { HeaderLanding } from "../organisms/headers/HeaderLanding";
+import GithubQR from "../../assets/images/GithubQR.png";
+export const Home = () => {
+  return (
+    <div className="home">
+      <div className="home__main">
+      <HeaderLanding />
+      <div className="home__visual">
+        <Link className="home__link" to={'/'}>
+          <img className="home__link__img" src={GithubQR} alt="Visit my GitHub profile" />
+        </Link>
+      </div>
+      </div>
+    </div>
+  );
+};
