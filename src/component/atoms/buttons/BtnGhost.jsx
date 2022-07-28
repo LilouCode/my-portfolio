5177c1750f7dabcf4ab8d3fe7bd5b21669f2184e
LilouCode <lisa.music@outlook.com>
2022-07-28 12:52:11 +0200

    feat: add mailto option to btn

--- src/component/atoms/buttons/BtnGhost.jsx
+++ src/component/atoms/buttons/BtnGhost.jsx
@@ -1,8 +1,13 @@
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
-export const BtnGhost = ({content = "button", link}) => {
+export const BtnGhost = ({content = "button", link, mailto}) => {
     return(
-        <Link to={link}>
+        <Link to={link} onClick={mailto? (
+            (e) =>{
+                window.location.href = mailto;
+                e.preventDefault();
+            }
+        ): null}>
             <button className="btn btn--ghost">{content}</button>
         </Link>
     )
@@ -10,4 +15,5 @@ export const BtnGhost = ({content = "button", link}) => {
 BtnGhost.propTypes = {
     content: PropTypes.string.isRequired,
     link: PropTypes.string.isRequired,
+    mailto: PropTypes.string
 }
--- src/component/atoms/buttons/BtnNav.jsx
+++ src/component/atoms/buttons/BtnNav.jsx
@@ -1,12 +1,18 @@
 import { NavLink } from "react-router-dom";
 import PropTypes from "prop-types";
-export const BtnNav = ({ content = "button", link }) => {
+export const BtnNav = ({ content = "button", link, mailto }) => {
   return (
     <NavLink
       to={link}
       className={({ isActive }) =>
         isActive ? "btn btn--nav btn--nav--active" : "btn btn--nav"
       }
+      onClick={mailto? (
+        (e) =>{
+          window.location.href = mailto;
+          e.preventDefault();
+      }
+      ): null}
     >
       {content}
     </NavLink>
@@ -15,4 +21,5 @@ export const BtnNav = ({ content = "button", link }) => {
 BtnNav.propTypes = {
   content: PropTypes.string.isRequired,
   link: PropTypes.string.isRequired,
+  mailto: PropTypes.string,
 };
