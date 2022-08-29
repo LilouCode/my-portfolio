e5dacc9de3d4b411ec9951028db22c6d3e363e72
LilouCode <lisa.music@outlook.com>
2022-08-29 14:10:12 +0200

    fix: provider never used

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Provider } from "react-redux";
+// import { Provider } from "react-redux";
 // import { store } from "./store/store";
 import App from './App';
 import { Home } from "./component/pages/Home";
