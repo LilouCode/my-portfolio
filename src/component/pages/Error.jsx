2e1b957e84fe3716bf232565fe29b185599961a3
LilouCode <lisa.music@outlook.com>
2022-08-29 17:43:59 +0200

    feat: implement 404 error page

--- src/component/pages/Error.jsx
+++ src/component/pages/Error.jsx
@@ -1,3 +1,25 @@
-export const Error = () =>{
-    
-}
+import { Page } from "../templates/Page";
+import ErrorSpace from "../../assets/images/ErrorSpace.svg";
+import { BtnGhost } from "../atoms/buttons/BtnGhost";
+export const Error = () => {
+  return (
+    <Page
+      chilidren={
+        <main className="error">
+          <div className="error__header">
+            <h1>
+              <img role="alert" src={ErrorSpace} alt="404 error" />
+            </h1>
+            <div className="error__body">
+            <div className="error__body__description">
+            <h2>Lost in space</h2>
+            <p>You have reached the end of the universe.</p>
+            </div>
+            <BtnGhost link="/" content="Back to home" />
+            </div>
+          </div>
+        </main>
+      }
+    />
+  );
+};
